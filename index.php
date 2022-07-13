e56d5601ac87bb6554a7e53f0ee5ea9cd7eb8986
MarcSa <marc@marcvirtual.com>
2022-07-13 10:58:39 +0200

    dashboard widget

--- index.php
+++ index.php
@@ -37,6 +37,7 @@ include( 'process/create-account.php' );
 include( 'process/login.php' );
 include( 'includes/shortcodes/auth-alt-form.php' );
 include( 'includes/front/logout-link.php' );
+include( 'includes/admin/dashboard-widgets.php' );
 
 
 // Hooks
@@ -61,6 +62,7 @@ add_action( 'wp_ajax_nopriv_recipe_user_login', 'recipe_user_login' );
 // add_filter( 'authenticate', 'wp_authenticate_spam_check', 99 );
 // add_filter( 'authenticate', 'r_alt_autheticate', 100, 3 );
 add_filter( 'wp_nav_menu_secondary_items', 'mv_new_nav_menu_items', 999 );
+add_action( 'wp_dashboard_setup', 'r_dashboard_widgets' );
 
 // Shortcodes
 add_shortcode( 'recipe_creator', 'r_recipe_creator_shortcode' );
--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,9 @@
 <?php
 
 if ( !defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-    exit;
+    die;
 }
 
 global $wpdb;
 
-$wpdb->query( "DROP TABLE IF EXISTS {$wpdb->prefix}recipe_ratings'" );
+$wpdb->query( "DROP TABLE IF EXISTS `" . $wpdb->prefix . "recipe_ratings`" );
