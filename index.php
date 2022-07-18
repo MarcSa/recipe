497e2cfa241a93f4151bb922b6551e01365bce20
MarcSa <marc@marcvirtual.com>
2022-07-18 13:24:34 +0200

    setting without errors

--- index.php
+++ index.php
@@ -42,7 +42,6 @@ include( 'includes/shortcodes/twitter-follow.php' );
 include( 'includes/admin/menus.php' );
 include( 'includes/admin/options-page.php' );
 include( 'process/save-options.php' );
-include( 'includes/admin/settings-api.php' );
 
 
 // Hooks
